cb5fc4c6925a0612f0ae73896b0987b9720b0a5a
DankSouls2002 <65518518+DankSouls2002@users.noreply.github.com>
2022-05-17 14:00:03 -0700

    Add files via upload

---
+++ Final Project/src/Player.java
@@ -0,0 +1,93 @@
+
+public class Player {
+
+	//By Payton Bayron. More than open to suggestions for additions!
+	
+	private String name;
+	
+	private int healthMax;
+	private int manaMax;
+	private int staminaMax;
+	
+	private int healthCurrent;
+	private int manaCurrent;
+	private int staminaCurrent;
+	
+	// ---- Stat Maximum Getters and Setters ----
+	
+	// ---- Health Max ----
+	public int getHealthMax()
+	{
+		return healthMax;
+	}
+	
+	public void setHealthMax(int newHealthMax)
+	{
+		healthMax = newHealthMax;
+	}
+	// ---- Mana Max ----
+	public int getManaMax()
+	{
+		return manaMax;
+	}
+	
+	public void setManaMax(int newManaMax)
+	{
+		manaMax = newManaMax;
+	}
+	// ---- Stamina Max ----
+	public int getStaminaMax()
+	{
+		return staminaMax;
+	}
+	
+	public void setStaminaMax(int newStaminaMax)
+	{
+		staminaMax = newStaminaMax;
+	}
+	
+	// ---- Stat Current Getters and Setters ----
+	
+	// ---- Health Current ----
+	public int getHealthCurrent()
+	{
+		return healthCurrent;
+	}
+	
+	public void setHealthCurrent(int newHealthCurrent)
+	{
+		healthMax = newHealthCurrent;
+	}
+	// ---- Mana Current ----
+	public int getManaCurrent()
+	{
+		return manaCurrent;
+	}
+	
+	public void setManaCurrent(int newManaCurrent)
+	{
+		manaCurrent = newManaCurrent;
+	}
+	// ---- Stamina Max ----
+	public int getStaminaCurrent()
+	{
+		return staminaCurrent;
+	}
+	
+	public void setStaminaCurrent(int newStaminaCurrent)
+	{
+		staminaMax = newStaminaCurrent;
+	}
+	
+	// ---- Name ----
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String newName)
+	{
+		name = newName;
+	}
+}
